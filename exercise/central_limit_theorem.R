397066a06dfe4637b327cb6e401f01845f2c79aa
jiaweih <jiaweihe@Jiaweis-MacBook-Pro.local>
2017-04-21 00:03:09 -0700

    code samples for statistics

---
+++ exercise/central_limit_theorem.R
@@ -0,0 +1,23 @@
+#
+# Explore the Central Limit Theorem.
+# The Central Limit Theorem: This states that if you 1) sample n times from 
+# a distribution, 2) calculate the mean, and 3) repeat that 1000 or so times, 
+# the collection of your estimates of the mean will become more normally 
+# distributed as the sample size n increases. This is true regardless of the 
+# shape of the distribution from which you are sampling.
+#
+
+pdf("central_limit_theorem.pdf", width=7, height=5)
+par(mfrow=c(2,3))
+
+for (lambda_ in c(0.1, 5)) {
+    for (n_ in c(10, 100, 1000)) {
+        sample.mean <- vector(length = 1000)
+        for (i in 1:1000) {
+            sample.mean[i] <- mean(rpois(n = n_, lambda = lambda_))
+        }
+        hist(sample.mean, main = paste("lambda=", lambda_,", sample_size=", n_))
+    }
+}
+
+dev.off()
---
+++ exercise/poisson.R
@@ -0,0 +1,37 @@
+#
+# Explore Poisson distributions.
+#
+
+library(ggplot2)
+
+dpoi <- function(lambda, k) {
+    # The probability mass function of the Poisson distribution
+    p <- lambda ** k * exp(-lambda) / factorial(k)
+    return(p)
+}
+
+
+dpoi.to.dataframe <- function(lambda) {
+    # Calculate Poisson probablity of a given lambda 
+    # against multiple k and save it to a data frame.
+    p <- vector(length = 20)
+
+    # Calculate the probability of X=k given lambda=1, for k=1:20. 
+    for (i in 1:20) {
+        p[i] <- dpoi(lambda, i)
+    }
+    # Deposit the vector p into a data frame
+    p.data.frame <- data.frame(p=p, k=1:20, lambda=lambda)
+    return(p.data.frame)
+}
+
+# Make three data frames for lambda=1, 4, 10.
+pois.dataframe <- do.call(rbind, lapply(c(1, 4, 10), dpoi.to.dataframe))
+pois.dataframe$lambda <- factor(pois.dataframe$lambda)
+
+pdf("poisson.pdf", width=7, height=5)
+# Plot p against k with both points and lines.
+ggplot(data=pois.dataframe, aes(x=k, y=p, fill=lambda, group=lambda, 
+    shape=lambda, colour=lambda)) + geom_line() + geom_point() + 
+    theme(legend.position=c(0.9,0.9))
+dev.off()
